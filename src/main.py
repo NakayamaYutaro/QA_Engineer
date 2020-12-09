50eb799d0ec047e82537c8dcb11295c8fba8b7dc
Yutaro Nakayama <170808naka@gmail.com>
2020-12-09 23:54:10 +0900

    add main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.style.use('ggplot')
+timenow = datetime.datetime.now()
+
+def main():
+    print(timenow)
+
+if __name__ == '__main__':
+    main()
